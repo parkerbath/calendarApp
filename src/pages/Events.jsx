e90a00f90d9e2c66937f094878a7be47cf14821b
parkerbath <pbath@lion.lmu.edu>
2021-04-05 14:12:59 -0700

    updated Events page

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import EventForm from "../components/EventForm";
+import EventList from "../components/EventList";
 
 export default function Events() {
+  const [events, setEvents] = useState([]);
+  function addToList(item) {
+    setEvents([{ ...item }, ...events]);
+    console.log(events);
+  }
   return (
     <div>
-      <EventForm />
+      <EventForm addToList={addToList} />
+      <EventList events={events} />
     </div>
   );
 }
